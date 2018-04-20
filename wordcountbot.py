eac627310f43d47a05251b01db34b399f6495041
tiotdev <tiotdev@tiot.de>
2018-04-20 16:09:27 +0000

    Improved error handling

--- wordcountbot.py
+++ wordcountbot.py
@@ -6,6 +6,7 @@ from steem.post import Post
 import json
 import datetime
 import re
+import time
 from bs4 import BeautifulSoup
 from markdown import markdown
 
@@ -21,7 +22,7 @@ def stream_blockchain():
         try:
             for post in stream:
                 tags = post["tags"]
-                if post.is_main_post() and "life" in tags:
+                if post.is_main_post() and "travelfeed" in tags:
                     author = post["author"]
                     body = post["body"]
                     html = markdown(body)
@@ -31,13 +32,19 @@ def stream_blockchain():
                     if count > 249:
                       print("There is a new awesome post by @{} who posted with {} words".format(author, count))
                     else:
+                        commenttext = "Hey {}, I am a helpful word counting bot and just found your post while running a test. I wanted to let you know that I have counted **{} words** in your post"
                         try:
-                            post.reply("Hey {}, I am a helpful word counting bot and just found your post while running a test. I wanted to let you know that I have counted **{} words** in your post".format(author, count), "", "jpphoto")
-                            print("I detected a rule breaker and sucessfully left a comment for @{} since we require at least 250 words but I have only counted {} words in the post".format(author, count))                    
-                        except assert_exception:
-                            print("20 second limit problem")
+                            post.reply(commenttext.format(author, count), "", "jpphoto")
+                            print("I detected a rule breaker and sucessfully left a comment for @{} since we require at least 250 words but I have only counted {} words in the post".format(author, count))              
                         except:
-                            print("Error")
+                            print("I detected a rule breaker but there was an error posting the comment, could be due to the 20 second limit on comments. I will try my best to not let him get away!")
+                            time.sleep(15)
+                            try:
+                                post.reply(commenttext.format(author, count), "", "jpphoto")
+                                print("Cool, now it worked!")
+                            except:
+                                print("Nope, still an error, this rulebreaker got away...")
+
         except Exception as error:
             print(repr(error))
             continue

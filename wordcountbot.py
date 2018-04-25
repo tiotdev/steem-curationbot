353e6cdadfa05af744ba9c6ec73b7bdae64beae3
Julian Peters <dev@tiot.de>
2018-04-25 22:18:04 +0200

    Added language processing

--- wordcountbot.py
+++ wordcountbot.py
@@ -11,6 +11,7 @@ import random
 from bs4 import BeautifulSoup
 from markdown import markdown
 from steembase.exceptions import PostDoesNotExist
+from langdetect import detect
 import atexit
 # Account to track for blacklisted/muted users
 trackaccount = 'travelfeed'
@@ -25,7 +26,9 @@ abusersmax = 20
 # Comment for short posts 
 shortposttext = "Hi @{}, \n Thank you for participating in the #travelfeed curated tag. To maintain a level of quality on the project we have certain criteria that must be met for participation. Please review the following: https://steemit.com/travelfeed/@travelfeed/how-to-participate-use-travelfeed-in-your-posts \n **We require at least 250 words, but your post has only {} words.** \n Thank you very much for your interest and we hope to read some great travel articles from you soon! \n Regards, TravelFeed"
 # Comment for blacklisted users
-blacklisttext = "Hi @{}, \n Thank you for participating in the #travelfeed curated tag. To maintain a level of quality on the project we have certain criteria that must be met for participation. Please review the following: https://steemit.com/travelfeed/@travelfeed/how-to-participate-use-travelfeed-in-your-posts \n Regards, TravelFeed"
+blacklisttext = "Hi @{}, \n Thank you for participating in the #travelfeed curated tag. To maintain a level of quality on the project we have certain criteria that must be met for participation. Please review the following: https://steemit.com/travelfeed/@travelfeed/how-to-participate-use-travelfeed-in-your-posts \n **You are currently blacklisted from the TravelFeed curation.** \n This is most likely because we have detected plagiarism in one of your posts in the past. If you believe that this is a mistake, please contact us on the [Steemit Travellers Discord](https://discord.gg/jWWu73H). c \n Regards, TravelFeed"
+# Comment for other languages
+wronglangtext = "Hi @{}, \n Thank you for participating in the #travelfeed curated tag. To maintain a level of quality on the project we have certain criteria that must be met for participation. Please review the following: https://steemit.com/travelfeed/@travelfeed/how-to-participate-use-travelfeed-in-your-posts \n **We require at least 250 words in English, but your post seems to be in another language.** (The language of your post was automatically detected, if your post is in English, please ignore this message.) \n Thank you very much for your interest and we hope to read some great travel articles from you soon! \n Regards, TravelFeed"
 # Define path for logging
 logpath = 'short_posts.log'
 try: 
@@ -73,58 +76,59 @@ def stream_blockchain(starting_point):
                 if post.is_main_post() and tracktag in tags:
                     permlink = post['permlink']
                     author = post["author"]
+                    commenttext = ""
                     if permlink in post_urls:
                         print(time.strftime('%X')+" Info: Ignoring updated post")
                     elif author in whitelist:
-                        print(time.strftime('%X')+" Info: Ignoring short post by whitelisted user")
+                        print(time.strftime('%X')+" Info: Ignoring short post by whitelisted user @{}".format(author))
                     elif any(d['following'] == author for d in abusers): 
-                        try: 
-                            post.reply(blacklisttext.format(author), "", postaccount)
-                            print(time.strftime('%X')+" Success: I detected a blacklisted user and sucessfully left a comment for @{}".format(author))              
-                        except:
-                            print(time.strftime('%X')+" Info: I detected a blacklisted user but there was an error posting the comment, could be due to the 20 second limit on comments. I will try my best to not let him get away!")
-                            time.sleep(19)
-                            try:
-                                post.reply(blacklisttext.format(author), "", postaccount)
-                                print(time.strftime('%X')+" Success: Cool, now it worked, I successfully left a comment for the blaklisted user @{}!".format(author))
-                            except:
-                                print(time.strftime('%X')+" Warning: Nope, still an error, this blacklisted user (@{}) got away due to the error".format(author)+repr(error))
+                        commenttext = blacklisttext
+                        print(time.strftime('%X')+" Success: Detected post by blacklisted user @{}".format(author))
                     else:
                         try:
-                            post_urls.append(permlink)
                             html = markdown(post["body"])
                             soup = BeautifulSoup(html, "html.parser")
                             text = ''.join(soup.findAll(text=True))
                             remlink = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
                             count = len(re.findall(r'\w+', remlink))
-                            if count > 249:
-                                print(time.strftime('%X')+" Success: There is a new awesome post by @{} who posted with {} words".format(author, count))
-                            else:
-                                try:
-                                    post.reply(shortposttext.format(author, count), "", postaccount)
-                                    print(time.strftime('%X')+" Success: I detected a rule breaker and sucessfully left a comment for @{} since we require at least 250 words but I have only counted {} words in the post".format(author, count))              
-                                except:
-                                    print(time.strftime('%X')+" Info: I detected a rule breaker but there was an error posting the comment, could be due to the 20 second limit on comments. I will try my best to not let him get away!")
-                                    time.sleep(19)
-                                    try:
-                                        post.reply(shortposttext.format(author, count), "", postaccount)
-                                        print(time.strftime('%X')+" Success: Cool, now it worked, I successfully left a comment for @{}!".format(author))
-                                    except:
-                                        print(time.strftime('%X')+" Warning: Nope, still an error, the rulebreaker (@{}) got away due to this error: ".format(author)+repr(error))
-                                        continue
+                            try:
+                                lang = detect(remlink)
+                            except:
+                                lang = "en"
+                            if not lang == "en":
+                                commenttext = wronglangtext
+                            elif count < 250:
+                                commenttext = shortposttext
+                                print(time.strftime('%X')+" Success: Detected short post by @{} who posted with just {} words".format(author, count))
                         except Exception as error:
-                            print(time.strftime('%X')+" Warning: Error while processing post "+repr(error))
+                            print(time.strftime('%X')+" Warning: Error during content processing "+repr(error))
                             continue
+                    if not commenttext == "":
+                        try:
+                            post.reply(commenttext.format(author, count), "", postaccount)
+                            print(time.strftime('%X')+" Success: I sucessfully left a comment for @{}".format(author))              
+                        except:
+                            print(time.strftime('%X')+" Info: There was an error posting the comment, could be due to the 20 second limit on comments. I will try again.")
+                            time.sleep(19)
+                            try:
+                                post.reply(commenttext.format(author, count), "", postaccount)
+                                print(time.strftime('%X')+" Success: Cool, now it worked, I successfully left a comment for @{}!".format(author))
+                            except:
+                                print(time.strftime('%X')+" Warning: Nope, still an error, I could not levae a comment for (@{}) due to this error: ".format(author)+repr(error))
+                                continue
+                    else:
+                        print(time.strftime('%X')+" Info: Ignoring awesome post by @{}".format(author))
+                    post_urls.append(permlink)
         except PostDoesNotExist:
-            print(time.strftime('%X')+" Info: Skipping invalid post")
+            print(time.strftime('%X')+" Info: Skipping blockchain error")
             continue
         except Exception as error:
-            print(time.strftime('%X')+" Warning: "+repr(error))
             try:
                 props = steem.get_dynamic_global_properties()
                 starting_point = props['last_irreversible_block_num']
             except:
                 starting_point = None
+            print(time.strftime('%X')+" Warning at block "+str(starting_point)+": "+repr(error))
             stream_blockchain(starting_point)
 
 def exit_handler():
